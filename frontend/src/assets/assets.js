0d7bc603f34d8980eb4ab8cf1efff35a7af9703e
Divanshu Bhargava <divanshu0212@gmail.com>
2024-08-26 02:43:08 +0530

    commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,31 @@
+import shooter from '../ss banner_images/shooter.jpg'
+import puzzle from '../ss banner_images/puzzle.jpg'
+import racing from '../ss banner_images/racing.jpg'
+import casual from '../ss banner_images/casual.jpg'
+import action from '../ss banner_images/action.jpg'
+
+
+export const category_list = [
+    {
+        category_name: "Racing",
+        category_banner_image: racing
+    },
+    {
+        category_name: "Puzzle",
+        category_banner_image: puzzle
+    },
+    {
+        category_name: "Action",
+        category_banner_image: action
+    },
+    {
+        category_name: "Shooter",
+        category_banner_image: shooter
+    },
+    {
+        category_name: "Casual",
+        category_banner_image: casual
+    }]
+
+
+   
