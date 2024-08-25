c3b96748cbc21083cefed40ba6b22abf78a06670
Speedy2705 <163318944+Speedy2705@users.noreply.github.com>
2024-08-25 19:32:38 +0530

    commit

--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Welcome = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='bg-black'>
       <div className='flex gap-5 justify-end'>
         <div className='bg-[#2ECC71] right-0 rounded-md px-3 text-2xl py-2'>
           Login
         </div>
-        <div className='bg-[#2ECC71] right-0 rounded-md px-3 text-2xl py-2'>
+        <div className='bg-[#2ECC71] right-0 rounded-md px-3 text-2xl py-2 cursor-pointer' onClick={()=>navigate("/signup")}>
           Sign Up
         </div>
       </div>
       <div className='flex justify-center items-center size-100'>
-        <img src="../../../public/assets/LogoMakerCa-1724567992878.png" alt=""
+        <img src="/LogoMakerCa-1724589494272.png" alt=""
           className='size-[1000px]' />
       </div>
 
