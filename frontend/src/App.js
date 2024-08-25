c9b296cc5b494095a4e7bd4bde5f257ec02da545
Speedy2705 <163318944+Speedy2705@users.noreply.github.com>
2024-08-25 18:14:15 +0530

    react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
   );
 }
 
