f4abd0f87c85073bc037f15a3f9a44a599a1577b
Divanshu Bhargava <divanshu0212@gmail.com>
2024-08-25 19:44:13 +0530

    commit

--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -11,12 +11,12 @@ const Welcome = () => {
         <div className='bg-[#2ECC71] right-0 rounded-md px-3 text-2xl py-2 mt-3' onClick={()=>navigate('/login')}>
           Login
         </div>
-        <div className='bg-[#2ECC71] right-0 rounded-md px-3 text-2xl py-2'>
+        <div className='bg-[#2ECC71] right-0 rounded-md px-3 text-2xl py-2 mt-3'>
           Sign Up
         </div>
       </div>
       <div className='flex justify-center items-center size-100'>
-        <img src="../../../public/assets/LogoMakerCa-1724567992878.png" alt=""
+        <img src="/LogoMakerCa-1724589494272.png" alt=""
           className='size-[1000px]' />
       </div>
 
